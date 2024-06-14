e385ee08eb9be42b7738f808db5b8c04f5b01b0f
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-14 18:49:43 -0400

    more react projects

---
+++ React-Projects/project-2/src/App.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import Navigation from './Components/Navigation/Navigation';
+import ContactHeader from './Components/ContactHeader/ContactHeader';
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <Navigation />
+      <ContactHeader />
+    </div>
+  )
+    
+  
+}
+
+export default App;
---
+++ React-Projects/project-2/src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "./ContactForm.module.css"
+
+const ContactForm = () => {
+  return (
+    <section className = {styles.container}
+    className = {styles.container}>
+        <div className = "">
+
+        </div>
+        <div>
+            <img src = "/public/Images/support.svg"/>
+        </div>
+    </section>
+  )
+}
+
+export default ContactForm
