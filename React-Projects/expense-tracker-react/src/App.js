e084fc6aefc47d9310efb57cb7f7373e368cfe99
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-14 18:46:10 -0400

    Added all the components for the expense tracker

---
+++ React-Projects/expense-tracker-react/src/App.js
@@ -0,0 +1,29 @@
+
+import React from "react"
+import Header from "./components/Header";
+import Balance from "./components/Balance"
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+import "./App.css"
+
+function App() {
+  console.log('App component rendered');
+  return (
+    <div >
+      <Header />
+      <div className = "container">
+      <Balance />
+      
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+        
+      
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ React-Projects/expense-tracker-react/src/context/GlobalState.js
@@ -0,0 +1,5 @@
+import React, {createContext, useReducer} from "react";
+
+//Initial state
+
+const initialState = {}
