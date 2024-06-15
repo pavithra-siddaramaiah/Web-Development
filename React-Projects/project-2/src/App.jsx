f11c921b243e80a619db98001572b2490a496cee
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-15 17:11:59 -0400

    updated App.jsx

--- React-Projects/project-2/src/App.jsx
+++ React-Projects/project-2/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import ContactForm from "./components/ContactForm/ContactForm";
-import ContactHeader from "./components/ContactHeader/ContactHeader";
-import Navigation from "./components/Navigation/Navigation";
+import ContactForm from "./Components/ContactForm/ContactForm";
+import ContactHeader from "./Components/ContactHeader/ContactHeader"
+import Navigation from "./Components/Navigation/Navigation"
 
 function App() {
   return (
