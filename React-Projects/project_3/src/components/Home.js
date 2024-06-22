7f5b6c746f11fef07debf88e71228e8df10f244c
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-21 20:47:40 -0400

    defined Home and Navbar components

---
+++ React-Projects/project_3/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {useState} from 'react'
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+  return (
+    <div className="home">
+        <h2>Home Page</h2>
+        
+        <button>Click Me</button>
+        
+    </div>
+  )
+}
+
+export default Home
