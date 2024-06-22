3a68ad35b5af0d0c168e636906b546b9ab6051cd
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-21 20:48:56 -0400

    updated AddTransaction

--- React-Projects/expense-tracker-react/src/components/AddTransaction.jsx
+++ React-Projects/expense-tracker-react/src/components/AddTransaction.jsx
@@ -3,14 +3,14 @@ import React, {useState} from 'react'
 export const AddTransaction = () => {
 
     const [text, setText] = useState('');
-    const[amount, setAmount] = useState(0);
+    const [amount, setAmount] = useState(0);
   return (
     <div>
         <h3>Add new transaction</h3>
         <form >
             <div Name = "form-control">
-                <label html = "text">Text</label>
-                <input type = " text" value={text} onChange = {(e) => setText(e.target.value)} placeholder='Enter text..'/>
+                <label html = "text">Text </label>
+                <input type = "text" value = {text} onChange = {(e) => setText(e.target.value)} placeholder='Enter text..'/>
             </div>
             <div className ="form-control">
                 <label htmlfor = "amount">Amount <br />
