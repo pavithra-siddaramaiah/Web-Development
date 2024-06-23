5d2bc709c4e7c6a8c91b94b977ee39c50f088f6d
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-22 22:21:06 -0400

    Fetching data with useState

--- React-Projects/project_3/src/components/BlogList.js
+++ React-Projects/project_3/src/components/BlogList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title}) => {
     // const blogs = props.blogs;
     // const title = props.title;
 
@@ -12,7 +12,7 @@ const BlogList = ({blogs, title, handleDelete}) => {
           <div className="blog-preview" key = {blog.id}>
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
-            <button onClick = {() => handleDelete(blog.id)}>Delete Blog</button>
+            {/* <button onClick = {() => handleDelete(blog.id)}>Delete Blog</button> */}
           </div>
 
         ))}
