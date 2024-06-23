5ba3c070b54582a1cf0591428aa69e4d46707e71
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-22 22:35:51 -0400

    added the Loading message to the user while fetching the data

--- React-Projects/project_3/src/components/Home.js
+++ React-Projects/project_3/src/components/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList'
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true);
 
     // const handleDelete = (id) => {
     //   const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -13,17 +14,22 @@ const Home = () => {
     
 
     useEffect(() => {
-      fetch('http://localhost:8000/blogs')
+      setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
       .then(res => {
        return res.json();
       } )
       .then(data => {
         setBlogs(data);
-      });
+        setIsPending(false);
+      })
+      }, 1000);
+      
     }, []);
 
   return (
     <div className="home">
+        { isPending && <div>Loading...</div> }
         {blogs && <BlogList blogs ={blogs} title = "All Blogs"  />}
         {/* <BlogList blogs = {blogs.filter((blog) => blog.author === "mario" )} title = "Mario Blogs" /> */}
         
