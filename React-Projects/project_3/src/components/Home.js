3b958eb7be84128beac5a0ad441971d415c84beb
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-28 21:12:01 -0400

    added catch and setError functionality

--- React-Projects/project_3/src/components/Home.js
+++ React-Projects/project_3/src/components/Home.js
@@ -5,6 +5,7 @@ import BlogList from './BlogList'
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     // const handleDelete = (id) => {
     //   const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -17,11 +18,22 @@ const Home = () => {
       setTimeout(() => {
         fetch('http://localhost:8000/blogs')
       .then(res => {
+        // console.log(res)
+        if(!res.ok){
+          throw Error("could not fetch the data from the resources");
+        }
        return res.json();
       } )
       .then(data => {
         setBlogs(data);
         setIsPending(false);
+
+
+      })
+      .catch((err) => {
+        // console.log(err.message)
+        setIsPending(false);
+        setError(err.message);
       })
       }, 1000);
       
@@ -29,6 +41,7 @@ const Home = () => {
 
   return (
     <div className="home">
+        {error && <div> {error} </div>}
         { isPending && <div>Loading...</div> }
         {blogs && <BlogList blogs ={blogs} title = "All Blogs"  />}
         {/* <BlogList blogs = {blogs.filter((blog) => blog.author === "mario" )} title = "Mario Blogs" /> */}
