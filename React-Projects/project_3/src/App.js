0dac8b480c7e60b63fa0ed7559049e46fbdd833b
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-28 23:58:28 -0400

    Added React Router functionality

--- React-Projects/project_3/src/App.js
+++ React-Projects/project_3/src/App.js
@@ -2,17 +2,22 @@
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
-      
-        
+        <Switch>
+          <Route path = "/">
+            <Home />
+        </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
