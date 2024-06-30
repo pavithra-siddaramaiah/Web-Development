a20bdf9910a57a4928d073df445df5de6480c93f
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-06-29 22:04:57 -0400

    Addeded delete button to delete the blog

--- React-Projects/project_3/src/components/BlogDetails.js
+++ React-Projects/project_3/src/components/BlogDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
   const { id } = useParams();
 
   const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+ fetch('http://localhost:8000/blogs/' + blog.id, {
+    method: 'DELETE',
+    //it is asynchronous and will fire a function when it executes hen we will use the then function
+ }).then(() => {
+    history.push('/')
+ })
+  }
 
   return (
     <div className="blog-details">
@@ -15,6 +26,7 @@ const BlogDetails = () => {
             <h2>{ blog.title}</h2>
             <p>Written by { blog.author }</p>
             <div>{ blog.body }</div>
+            <button onClick = {handleClick}>delete</button>
         </article>
         
       )}
