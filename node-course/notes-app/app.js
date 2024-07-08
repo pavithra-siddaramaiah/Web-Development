eb7bde225f435210ab4f4f89ac8642e395f84271
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-08 16:57:58 -0400

    updated add

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -2,7 +2,52 @@ const getNotes = require('./notes')
 const chalk = require('chalk')
 const yargs = require('yargs')
 
-console.log(process.argv[2])
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Added new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            description: 'Body of note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Adding the notes..\n'+ 'title: '+ argv.title +'\n'+ 'body: '+argv.body)
+    }
+})
 
-console.log(yargs.argv)
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove note',
+    handler: function () {
+        console.log('Removing the notes..')
+    }
+})
 
+//create a list command
+yargs.command({
+    command: 'list',
+    describe: 'notes list',
+    handler: function () {
+        console.log('showing all the notes..')
+    }
+})
+
+//create a read command
+yargs.command({
+    command: 'read',
+    describe: 'Read note',
+    handler: function () {
+        console.log('Reading the notes..')
+    }
+})
+
+yargs.parse();
