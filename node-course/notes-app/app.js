3523f74c21a41ac990a96e7da31dd763090589e0
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-08 13:52:35 -0400

    started a new course on node

---
+++ node-course/notes-app/app.js
@@ -0,0 +1,8 @@
+const getNotes = require('./notes')
+const chalk = require('chalk')
+const yargs = require('yargs')
+
+console.log(process.argv[2])
+
+console.log(yargs.argv)
+
---
+++ node-course/notes-app/notes.js
@@ -0,0 +1,6 @@
+
+const getNotes = () => {
+    return "Yoou Notes.."
+}
+
+module.exports = getNotes
