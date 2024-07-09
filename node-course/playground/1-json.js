abf8cddf627e60803bd5ac82a034df06d3a39953
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-08 23:32:55 -0400

    Used all the string and JSON operations

--- node-course/playground/1-json.js
+++ node-course/playground/1-json.js
@@ -4,9 +4,18 @@ const fs = require('fs')
 //     author: "abc"
 // }
 
-// const bookJSON = JSON.stringify(book)
-// fs.writeFileSync('1-json.json', bookJSON)
-const dataBuffer = fs.readFileSync('1-json.json')
-// console.log(dataBuffer.toString())
-const data = dataBuffer.toString()
-console.log(JSON.parse(data).title)
+// // const bookJSON = JSON.stringify(book)
+// // fs.writeFileSync('1-json.json', bookJSON)
+// const dataBuffer = fs.readFileSync('1-json.json')
+// // console.log(dataBuffer.toString())
+// const data = dataBuffer.toString()
+// console.log(JSON.parse(data).title)
+
+const parsedData = fs.readFileSync('1-json.json')
+const data = JSON.parse(parsedData.toString())
+console.log(data)
+data.name = "pavithra"
+data.age = 25
+const updatedData = JSON.stringify(data)
+fs.writeFileSync('1-json.json', updatedData)
+console.log(fs.readFileSync('1-json.json').toString())
