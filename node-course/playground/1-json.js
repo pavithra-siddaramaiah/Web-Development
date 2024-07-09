eb4bce749f11713f46500e7586288cb7b4551ab6
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-08 23:08:49 -0400

    Using toString method and parse

--- node-course/playground/1-json.js
+++ node-course/playground/1-json.js
@@ -1,10 +1,12 @@
-const book = {
-    title: "Hello World",
-    author: "abc"
-}
+const fs = require('fs')
+// const book = {
+//     title: "Hello World",
+//     author: "abc"
+// }
 
-const bookJSON = JSON.stringify(book)
-console.log(bookJSON)
-
-const parsedData = JSON.parse(bookJSON)
-console.log(parsedData)
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+const dataBuffer = fs.readFileSync('1-json.json')
+// console.log(dataBuffer.toString())
+const data = dataBuffer.toString()
+console.log(JSON.parse(data).title)
