454363ac62d241392da3c8410e7e97dee035dfec
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-11 11:17:34 -0400

    error functionality implemented

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,6 +1,19 @@
 const request = require('postman-request')
 
 
+
 request({url, json:true} ,(error, response) => {
-    console.log(response.body.main.humidity)
+    // Get it printed in terms of local errors like if network is not connected or any basic errors
+    if(error){
+        console.log("Check your connection and try again")
+    }
+    //This is popprd up incase of any mistakes/typo in the url
+    else if(response.body.error){
+        console.log('check the url and try accessing again')
+    }
+    //if everything is fine then print the response
+    else {
+        console.log(response.body.main.humidity)
+    }
+    
 })
